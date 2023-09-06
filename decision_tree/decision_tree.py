524bcdcf681d3eaede8e3d4fa4b90923b61992e2
unknown <robert.haug8@gmail.com>
2023-09-06 15:31:02 +0200

    predict now works

--- decision_tree/decision_tree.py
+++ decision_tree/decision_tree.py
@@ -24,10 +24,6 @@ class DecisionTree:
             y (pd.Series): a vector of discrete ground-truth labels
         """
 
-        print("--------------------------------------")
-        print("self.rules is currently:", self.rules)
-
-        # print(X)
         total_entropy = entropy(np.array(y.value_counts().tolist()))
         columns_infogain = np.zeros(len(X.columns))
 
@@ -50,78 +46,23 @@ class DecisionTree:
 
         for unique in root.unique():
 
-            print("currently processing:", root.name, ":", unique)
-            print("rules_backup is", rules_backup)
-            print("self.rules is", self.rules)
-
-            print("rules before backup:", rules)
-
             rules = list(rules_backup)
 
-            print("rules after backup:", rules)
-
             rules.append((root.name, unique))
-            print("rules_backup is now", rules_backup)
-            # print("RULES BEFORE", rules)
-            # print("THIS VALUE IS", unique)
 
             indices = root[root == unique].index.tolist()
-            # print(indices)
-            # print(y.iloc[indices])
-            # print("y:", y)
-            # print("indices:", indices)
-            # print("attempting to find", indices, "in", y)
-            # print("result:", y.loc[indices].value_counts())
             counts = np.array(y.loc[indices].value_counts().tolist())
-            # print("counts:", counts)
             unique_entropy = entropy(counts)
-            # print(unique_entropy)
+
             if unique_entropy == 0:
-                # print("y:", y)
-                # print("indices:", indices)
-                # rules = (rules, y[indices].iloc[0])
-                # print("entropy is zero")
-                # print("self.rules currently", self.rules)
-                # print("adding", (rules, y[indices].iloc[0]))
-                print("entropy is zero, rules are ", rules)
-                print("self.rules is ", self.rules)
-                print("adding", (rules, y[indices].iloc[0]), "to self.rules")
                 to_append = (list(tuple(rules)), y[indices].iloc[0])
                 self.rules.append(to_append)
-                # self.rules.append((rules, y[indices].iloc[0]))
-                print("self.rules is now", self.rules)
                 rules.pop()
-                print("entropy is zero")
-                print("self.rules is now", self.rules)
-                # print("entropy is zero, rules are now", self.rules)
-
-                # print("ENTROPY ZERO")
-                # print(unique)
-                # print(rules)
-                # rules.pop()
                 continue
             else:
-                # print("locating indices", indices)
-                # print("ENTROPY NONZERO")
-                # print(unique)
-                # print(rules)
                 X_new = X.loc[indices].drop(root.name,axis=1)
                 y_new = y[indices]
-                # print("hello")
-                print("entropy is nonzero")
-                print("self.rules is currently", self.rules)
-                # print(rules)
-                # self.fit(X_new,y_new,rules
-                print("going deeper. rules_backup is currently", rules_backup)
-                # rules_backup.pop()
                 self.fit(X_new,y_new,rules)
-                # print("hello")
-                print("Final rules:", self.rules)
-                print("///////////////////////////")
-
-        # print("hello")
-        # print(rules)
-
 
     def predict(self, X):
         """
@@ -137,8 +78,20 @@ class DecisionTree:
         Returns:
             A length m vector with predictions
         """
-        # TODO: Implement 
-        raise NotImplementedError()
+        results = []
+
+        rules = self.get_rules()
+
+        for i in range(len(X)):
+            # print(X.iloc[i])
+            data = []
+            for j in range(len(X.iloc[i])):
+                data.append((X.columns[j],X.iloc[i,j]))
+            for k in range(len(rules)):
+                if is_contained_within(data,rules[k][0]):
+                    results.append(rules[k][1])
+
+        return np.array(results)
     
     def get_rules(self):
         """
@@ -159,6 +112,15 @@ class DecisionTree:
         ]
         """
         return self.rules
+
+def is_contained_within(a,b):
+    a_set = set(a)
+
+    for item in b:
+        if item not in a_set:
+            return False
+
+    return True
 
 # --- Some utility functions 
     
