2b210fbcbc745a829211e7a165ec9b18d8b7f3be
unknown <robert.haug8@gmail.com>
2023-09-06 14:41:20 +0200

    holy shit i think it actually works

--- decision_tree/decision_tree.py
+++ decision_tree/decision_tree.py
@@ -6,12 +6,14 @@ import pandas as pd
 
 class DecisionTree:
     
-    def __init__():
+    def __init__(self):
         # NOTE: Feel free add any hyperparameters 
         # (with defaults) as you see fit
+        self.rules = []
+
         pass
     
-    def fit(self, X, y):
+    def fit(self, X, y, rules=[]):
         """
         Generates a decision tree for classification
         
@@ -21,9 +23,106 @@ class DecisionTree:
                 to the features.
             y (pd.Series): a vector of discrete ground-truth labels
         """
-        # TODO: Implement 
-        raise NotImplementedError()
-    
+
+        print("--------------------------------------")
+        print("self.rules is currently:", self.rules)
+
+        # print(X)
+        total_entropy = entropy(np.array(y.value_counts().tolist()))
+        columns_infogain = np.zeros(len(X.columns))
+
+        for i, column in enumerate(X):
+            column_infogain = total_entropy
+
+            for unique_value in X[column].unique():
+                indices = X[X[column] == unique_value].index.tolist()
+                counts = np.array(y.reindex(indices).value_counts().tolist())
+                value_entropy = entropy(counts)
+                prop = len(indices)/len(X.index)
+                column_infogain -= prop*value_entropy
+
+            columns_infogain[i] = column_infogain
+
+        index_of_highest_infogain = np.argmax(column_infogain)
+        root = X.iloc[:,index_of_highest_infogain]
+
+        rules_backup = tuple(rules)
+
+        for unique in root.unique():
+
+            print("currently processing:", root.name, ":", unique)
+            print("rules_backup is", rules_backup)
+            print("self.rules is", self.rules)
+
+            print("rules before backup:", rules)
+
+            rules = list(rules_backup)
+
+            print("rules after backup:", rules)
+
+            rules.append((root.name, unique))
+            print("rules_backup is now", rules_backup)
+            # print("RULES BEFORE", rules)
+            # print("THIS VALUE IS", unique)
+
+            indices = root[root == unique].index.tolist()
+            # print(indices)
+            # print(y.iloc[indices])
+            # print("y:", y)
+            # print("indices:", indices)
+            # print("attempting to find", indices, "in", y)
+            # print("result:", y.loc[indices].value_counts())
+            counts = np.array(y.loc[indices].value_counts().tolist())
+            # print("counts:", counts)
+            unique_entropy = entropy(counts)
+            # print(unique_entropy)
+            if unique_entropy == 0:
+                # print("y:", y)
+                # print("indices:", indices)
+                # rules = (rules, y[indices].iloc[0])
+                # print("entropy is zero")
+                # print("self.rules currently", self.rules)
+                # print("adding", (rules, y[indices].iloc[0]))
+                print("entropy is zero, rules are ", rules)
+                print("self.rules is ", self.rules)
+                print("adding", (rules, y[indices].iloc[0]), "to self.rules")
+                to_append = (list(tuple(rules)), y[indices].iloc[0])
+                self.rules.append(to_append)
+                # self.rules.append((rules, y[indices].iloc[0]))
+                print("self.rules is now", self.rules)
+                rules.pop()
+                print("entropy is zero")
+                print("self.rules is now", self.rules)
+                # print("entropy is zero, rules are now", self.rules)
+
+                # print("ENTROPY ZERO")
+                # print(unique)
+                # print(rules)
+                # rules.pop()
+                continue
+            else:
+                # print("locating indices", indices)
+                # print("ENTROPY NONZERO")
+                # print(unique)
+                # print(rules)
+                X_new = X.loc[indices].drop(root.name,axis=1)
+                y_new = y[indices]
+                # print("hello")
+                print("entropy is nonzero")
+                print("self.rules is currently", self.rules)
+                # print(rules)
+                # self.fit(X_new,y_new,rules
+                print("going deeper. rules_backup is currently", rules_backup)
+                # rules_backup.pop()
+                self.fit(X_new,y_new,rules)
+                # print("hello")
+                print("Final rules:", self.rules)
+                print("///////////////////////////")
+
+        # print("hello")
+        # print(rules)
+
+
     def predict(self, X):
         """
         Generates predictions
@@ -59,9 +158,7 @@ class DecisionTree:
             ...
         ]
         """
-        # TODO: Implement
-        raise NotImplementedError()
-
+        return self.rules
 
 # --- Some utility functions 
     
